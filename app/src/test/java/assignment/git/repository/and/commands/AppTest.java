7739351e76c4708d15e19f660b2e2eac5a05d4bf
Node Next <myphonecodyjoy@gmail.com>
2023-04-01 12:52:02 -0400

    Initial commit

---
+++ app/src/test/java/assignment/git/repository/and/commands/AppTest.java
@@ -0,0 +1,30 @@
+package assignment.git.repository.and.commands;
+import java.io.File;
+
+public class AppTest {
+    public static void main(String[] args) {
+        String[] folders = {"Desktop", "Downloads", "Documents"};
+        String needle = "Needle.txt";
+        String path = System.getProperty("user.home");
+        File[] files = new File(path).listFiles();
+        boolean needleFound = false;
+        for (String folder : folders) {
+            for (File file : files) {
+                if (file.isDirectory() && file.getName().equals(folder)) {
+                    File[] folderFiles = file.listFiles();
+                    for (File f : folderFiles) {
+                        if (f.isFile() && f.getName().equals(needle)) {
+                            System.out.println("Found the needle in " + folder + " folder!");
+                            needleFound = true;
+                        }
+                    }
+                }
+            }
+        }
+        if (!needleFound) {
+            System.out.println("Needle not found!");
+        } else {
+            System.out.println("Done searching!");
+        }
+    }
+}
